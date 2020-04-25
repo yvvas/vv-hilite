5a30e0631d66ec7510c98442ffc856ba337e15ac
John Maeda <jmaeda@appliedminds.com>
2020-04-24 19:29:32 -0700

    fixed selection based on input length

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 
-export default Vue.directive( 'v-hilite', {
-    update: ( el ) => {
-        el.focus()
-        el.select()
+export default Vue.directive('v-hilite', {
+    inserted: (el) => {
+        el['v-hilite'] = el.value
+    },
+    update: (el) => {
+        if(el.style.display === 'none') return
+        
+        const oldLength = el['v-hilite'].length
+        // console.log(oldLength + ':' + el.value.length)
+
+        if(oldLength + 1 === el.value.length || oldLength - 1 === el.value.length) {
+            // console.log('Don\'t stop')
+        } else if (el.value.length - 1 === 0) {
+            el['v-hilite'] = el.value
+        } else {
+            el.focus()
+            el.select()
+        }
+
+        el['v-hilite'] = el.value
     }
-} )
+})
